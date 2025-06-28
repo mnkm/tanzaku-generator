3553f1b587c493d6651dfada43f64efc30c23b96
mnkm <stk_mnkm@hotmail.com>
2025-06-28 14:35:01 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -79,7 +79,6 @@ $(async function () {
         canvas.height = img.height;
 
         const ctx = canvas.getContext('2d');
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.drawImage(img, 0, 0);
 
         const textAry = inputText.split('\n');
